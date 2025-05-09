29b3bc0bfde5755a30673f9ac9922f2b720fb9b3
CesarMCuellarCha <cesarmcuellar@gmail.com>
2025-05-09 12:26:55 -0500

    primeros ejercicios de estructuras de control

---
+++ EstructurasRepetitivas/ejer1.js
@@ -0,0 +1,38 @@
+//para
+let i=3
+for(let i=1; i<=20; i=i+1){
+    console.log(i*2)
+}
+
+
+i=1
+while(i<=20){
+    console.log(i*2)
+    i=i+1
+}
+
+i=1
+do{
+    console.log(i*2)
+    i=i+1
+}while(i<=20)
+
+for (let i=1;i<=10;i++){
+    if (i%2 === 1){
+        continue
+    }
+    console.log(i)
+}
+
+let x=4
+
+while(x<=10){
+    console.log(x)
+    x++
+    if(x===7)break
+}
+
+for (let i=10;i>=1;i--){
+    console.log(i)
+}
+
--- EstructurasdeControl/ejer0.js
+++ EstructurasdeControl/ejer0.js
@@ -1,1 +1,65 @@
-console.log("Hola")
+//const prompt = require('prompt-sync')();
+if (5>7){
+    console.log("Verdadero")
+}else{
+    console.log("Falso")
+}
+
+let dato="Cine"
+let estadoTiempo="verano"
+let verano=3
+if (estadoTiempo==="verano"){
+    console.log("voy a " + dato)
+}else{
+    console.log("me quedo en casa")
+}
+
+let x=10
+
+if(x>5){
+    console.log("Mayor")
+}
+
+{
+    let z=20
+}
+
+let valor=5
+
+if (valor===1) {
+    console.log("Uno")
+}
+if (valor===2) {
+    console.log("Dos")
+}
+if (valor===3) {
+    console.log("Tres")
+}
+if (valor===4) {
+    console.log("Cuatro")
+}
+if (valor===5) {
+    console.log("Cinco")
+}
+
+if (valor!==1 && valor!==2 && valor!==3 && valor!==4 && valor!==5){
+    console.log("Diferente valor de 1 a 5")
+}
+
+if (valor===1){
+    console.log("Uno")
+} else if(valor===2){
+    console.log("Dos")
+}else if(valor===3){
+    console.log("Tres")
+}else if(valor===4){
+    console.log("Cuatro")
+}else if(valor===5){
+    console.log("Cinco")
+}else{
+    console.log("otro valor")
+}
+
+//let nombre = prompt('¿Cuál es tu nombre? ');
+//console.log(`¡Hola, ${nombre}!`);
+
